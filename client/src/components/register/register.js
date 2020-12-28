c0ce42721de91fc70546033c99745de3e266dc8a
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-12-28 21:09:04 +0000

    add styling for login and registration pages

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -1,14 +1,45 @@
 function Register() {
     return (
-        <div className="h-screen-minus-navbar pb-14">
-            <h1>Register</h1>
-            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                </div>
+        <div className="h-screen-minus-navbar pb-14 grid grid-cols-1">
+            <div className="max-w-lg place-self-center">
                 <div>
-                    <div className="text-xl font-medium text-black">ChitChat</div>
-                    <p className="text-gray-500">You have a new message!</p>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Create a new account
+                    </h2>
                 </div>
+                <form className="mt-8 space-y-6" action="#" method="POST">
+                    <input type="hidden" name="remember" value="true"></input>
+                    <div className="rounded-md shadow-sm -space-y-px">
+                        <div>
+                            <label htmlFor="email-address" className="sr-only">Email address</label>
+                            <input id="email-address" name="email" type="email" autoComplete="email" required
+                                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                   placeholder="Email address"></input>
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="sr-only">Password</label>
+                            <input id="password" name="password" type="password" autoComplete="current-password"
+                                   required
+                                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                   placeholder="Password"></input>
+                        </div>
+                        <div>
+                            <label htmlFor="confirmPassword" className="sr-only">Confirm password</label>
+                            <input id="confirmPassword" name="confirmPassword" type="password" autoComplete="current-confirm-password"
+                                   required
+                                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                   placeholder="Confirm password"></input>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit"
+                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+          </span>
+                            Sign up
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )

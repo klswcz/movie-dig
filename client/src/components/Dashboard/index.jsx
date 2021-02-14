ba5f8ec5983e6fd50ea91ebc71588929fb223669
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-02-14 17:47:08 +0000

    add if else statements to components to display DOM only after receiveing API response

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 
 function Dashboard() {
     const [trendingMovies, setTrendingMovies] = useState([])
+    const [loadingFinished, setLoadingFinished] = useState(false)
 
     useEffect(() => {
         getTrendingMovies()
@@ -12,20 +13,27 @@ function Dashboard() {
     const getTrendingMovies = () => {
         trendingService().then(res => {
             setTrendingMovies(res.data.movies)
+            setLoadingFinished(true)
         }).catch(() => {
         })
     };
 
     return (
         <div className="h-screen-minus-navbar pb-14 px-4">
-            <h1 className="text-4xl mt-5">Trending today</h1>
-            <div className="grid xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
-                {trendingMovies.map((movie, index) => {
-                    return (
-                        <MovieCard title={movie.title ?? movie.name} voteAverage={movie.vote_average} posterPath={movie.poster_path} userRating={movie.user_rating} key={index} movieId={movie.id}/>
-                    )
-                })}
-            </div>
+            {loadingFinished ?
+                (<>
+                    <h1 className="text-4xl mt-5">Trending today</h1>
+                    <div className="grid xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 grid-cols-2">
+                        {trendingMovies.map((movie, index) => {
+                            return (
+                                <MovieCard title={movie.title ?? movie.name} voteAverage={movie.vote_average}
+                                           posterPath={movie.poster_path} userRating={movie.user_rating} key={index}
+                                           movieId={movie.id}/>
+                            )
+                        })}
+                    </div>
+                </>) :
+                <p>Loading</p>}
         </div>
     )
 }
--- client/src/components/Movie/index.jsx
+++ client/src/components/Movie/index.jsx
@@ -19,16 +19,17 @@ function Dashboard() {
     const [isWishlistItem, setIsWishlistItem] = useState(false)
     const [isWishlistRequestProcessed, setIsWishlistRequestProcessed] = useState(false)
     const [userRating, setUserRating] = useState(null)
+    const [loadingFinished, setLoadingFinished] = useState(false)
 
     useEffect(() => {
         setIsWishlistRequestProcessed(true)
-        movieInfoService({movieId: movieId}).then(res => {
-            setMovieInfo(res.data.movie)
-            setIsWishlistItem(res.data.isWishlistItem)
-            setIsWishlistRequestProcessed(false)
-
-            getRatingService({movieId: movieId}).then(res => {
-                setUserRating(res.data.rating)
+        getRatingService({movieId: movieId}).then(res => {
+            setUserRating(res.data.rating)
+            movieInfoService({movieId: movieId}).then(res => {
+                setMovieInfo(res.data.movie)
+                setIsWishlistItem(res.data.isWishlistItem)
+                setIsWishlistRequestProcessed(false)
+                setLoadingFinished(true)
             })
         })
     }, [movieId]);
@@ -64,72 +65,77 @@ function Dashboard() {
 
     return (
         <div className="h-screen-minus-navbar pb-14 px-4">
-            <h1 className="text-4xl mt-5">{movieInfo.title}</h1>
-            <p className="mb-5 text-gray-500">{movieInfo.tagline}</p>
-            <div className="flex flex-wrap">
-                <div className="w-full md:w-1/4">
-                    { movieInfo.poster_path &&
-                        <img src={'https://image.tmdb.org/t/p/w300' + movieInfo.poster_path} alt="" className="rounded-md"/>
-                    }
-                    <p className="my-2">
-                        your rating
-                    </p>
-                    <div className="inline-flex">
-                        <ReactStars
-                            className={'inline-block'}
-                            count={5}
-                            value={parseFloat(userRating)}
-                            onChange={setRating}
-                            size={24}
-                            color2={'#ffd700'}/>
-                        {userRating !== null &&
-                        <button onClick={deleteRating} className='inline-block ml-4'>
-                            <FontAwesomeIcon icon={faTrash}/>
-                        </button>
-                        }
+            {loadingFinished ?
+                (<>
+                    <h1 className="text-4xl mt-5">{movieInfo.title}</h1>
+                    <p className="mb-5 text-gray-500">{movieInfo.tagline}</p>
+                    <div className="flex flex-wrap">
+                        <div className="w-full md:w-1/4">
+                            {movieInfo.poster_path &&
+                            <img src={'https://image.tmdb.org/t/p/w300' + movieInfo.poster_path} alt=""
+                                 className="rounded-md"/>
+                            }
+                            <p className="my-2">
+                                your rating
+                            </p>
+                            <div className="inline-flex">
+                                <ReactStars
+                                    className={'inline-block'}
+                                    count={5}
+                                    value={parseFloat(userRating)}
+                                    onChange={setRating}
+                                    size={24}
+                                    color2={'#ffd700'}/>
+                                {userRating !== null &&
+                                <button onClick={deleteRating} className='inline-block ml-4'>
+                                    <FontAwesomeIcon icon={faTrash}/>
+                                </button>
+                                }
+                            </div>
+                            <p className="my-2">general rating</p>
+                            <ReactStars
+                                count={5}
+                                value={movieInfo.vote_average / 2}
+                                size={24}
+                                edit={false}
+                                color2={'#ffd700'}/>
+                            <button onClick={isWishlistItem ? deleteWishlistItem : addWishlistItem}
+                                    className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    disabled={isWishlistRequestProcessed}>
+                                {isWishlistItem ? 'Remove from wish list' : 'Add to wish list'}
+                            </button>
+                        </div>
+                        <div className="w-full md:w-3/4">
+                            {movieInfo.genres &&
+                            movieInfo.genres.map((genre, index) => {
+                                return (
+                                    <p className="inline-block pr-4" key={index}>{genre.name}</p>
+                                )
+                            })
+                            }
+                            <h2 className="text-2xl mt-5">Description</h2>
+                            <p>{movieInfo.overview}</p>
+                            <h2 className="text-2xl mt-5">Cast</h2>
+                            <div className="grid grid-cols-8">
+                                {movieInfo.credits &&
+                                movieInfo.credits.cast.slice(0, 8).map((actor, index) => {
+                                    return (
+                                        <div className="text-center flex items-center flex-col mr-10" key={index}>
+                                            {actor.profile_path &&
+                                            <img src={'https://image.tmdb.org/t/p/w300' + actor.profile_path} alt=""
+                                                 className="rounded-lg w-full"/>
+                                            }
+                                            <span className="font-bold block">{actor.name}</span>
+                                            <span className="block">as</span>
+                                            <span className="font-bold block">{actor.character}</span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
-                    <p className="my-2">general rating</p>
-                    <ReactStars
-                        count={5}
-                        value={movieInfo.vote_average / 2}
-                        size={24}
-                        edit={false}
-                        color2={'#ffd700'}/>
-                    <button onClick={isWishlistItem ? deleteWishlistItem : addWishlistItem}
-                            className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                            disabled={isWishlistRequestProcessed}>
-                        {isWishlistItem ? 'Remove from wish list' : 'Add to wish list'}
-                    </button>
-                </div>
-                <div className="w-full md:w-3/4">
-                    {movieInfo.genres &&
-                    movieInfo.genres.map((genre, index) => {
-                        return (
-                            <p className="inline-block pr-4" key={index}>{genre.name}</p>
-                        )
-                    })
-                    }
-                    <h2 className="text-2xl mt-5">Description</h2>
-                    <p>{movieInfo.overview}</p>
-                    <h2 className="text-2xl mt-5">Cast</h2>
-                    <div className="grid grid-cols-8">
-                        {movieInfo.credits &&
-                        movieInfo.credits.cast.slice(0, 8).map((actor, index) => {
-                            return (
-                                <div className="text-center flex items-center flex-col mr-10" key={index}>
-                                    {actor.profile_path &&
-                                    <img src={'https://image.tmdb.org/t/p/w300' + actor.profile_path} alt=""
-                                         className="rounded-lg w-full"/>
-                                    }
-                                    <span className="font-bold block">{actor.name}</span>
-                                    <span className="block">as</span>
-                                    <span className="font-bold block">{actor.character}</span>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
+                </>) :
+                <p>Loading</p>}
         </div>
     )
 }

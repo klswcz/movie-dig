8f53db4f5edda24ba089490bfe4ba15b42ec2470
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-02-22 14:19:27 +0000

    update movie info page to display properly on mobile devices

--- client/src/components/Movie/index.jsx
+++ client/src/components/Movie/index.jsx
@@ -73,56 +73,81 @@ function Dashboard() {
         <div className="h-screen-minus-navbar pb-14 px-4">
             {loadingFinished ?
                 (<>
-                    <h1 className="text-4xl mt-5">{movieInfo.title}</h1>
+                    <h1 className="text-4xl mt-5 sm:mb-3">{movieInfo.title}</h1>
+                    {movieInfo.tagline &&
                     <p className="mb-5 text-gray-500">{movieInfo.tagline}</p>
+                    }
+                    <div className="inline-block sm:hidden mb-3">
+                        {movieInfo.genres &&
+                        movieInfo.genres.map((genre, index) => {
+                            return (
+                                <p className="inline-block pr-4" key={index}>{genre.name}</p>
+                            )
+                        })
+                        }
+                    </div>
                     <div className="flex flex-wrap">
-                        <div className="w-full md:w-1/4">
+                        <div className="w-full sm:w-1/4">
                             {movieInfo.poster_path &&
                             <img src={'https://image.tmdb.org/t/p/w300' + movieInfo.poster_path} alt=""
-                                 className="rounded-md"/>
+                                 className="rounded-md w-1/3 align-top sm:align-middle sm:w-auto inline-block sm:block"/>
                             }
-                            <p className="my-2">
-                                your rating
-                            </p>
-                            <div className="inline-flex">
-                                <ReactStars
-                                    className={'inline-block'}
-                                    count={5}
-                                    value={parseFloat(userRating)}
-                                    onChange={setRating}
-                                    size={24}
-                                    color2={'#ffd700'}/>
-                                {userRating !== null &&
-                                <button onClick={deleteRating} className='inline-block ml-4'>
-                                    <FontAwesomeIcon icon={faTrash}/>
-                                </button>
-                                }
+                            <div className="w-2/3 pl-3 inline-block sm:hidden align-top">
+                                <h2 className="text-2xl">Description</h2>
+                                <p>{movieInfo.overview}</p>
                             </div>
-                            <p className="my-2">general rating</p>
-                            <ReactStars
-                                count={5}
-                                value={movieInfo.vote_average / 2}
-                                size={24}
-                                edit={false}
-                                color2={'#ffd700'}/>
+                            <div className="">
+                                <p className="my-2">
+                                    Your rating
+                                </p>
+                                <div className="inline-flex">
+                                    <ReactStars
+                                        className={'inline-block'}
+                                        count={5}
+                                        value={parseFloat(userRating)}
+                                        onChange={setRating}
+                                        size={24}
+                                        color2={'#ffd700'}/>
+                                    {userRating !== null &&
+                                    <button onClick={deleteRating} className='inline-block ml-4'>
+                                        <FontAwesomeIcon icon={faTrash}/>
+                                    </button>
+                                    }
+                                </div>
+                            </div>
+                            <div className="">
+                                <p className="my-2">Overall rating</p>
+                                <div className="inline-flex">
+                                    <ReactStars
+                                        count={5}
+                                        value={movieInfo.vote_average / 2}
+                                        size={24}
+                                        edit={false}
+                                        color2={'#ffd700'}/>
+                                </div>
+                            </div>
+
                             <button onClick={isWishlistItem ? deleteWishlistItem : addWishlistItem}
-                                    className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium block mt-2"
                                     disabled={isWishlistRequestProcessed}>
                                 {isWishlistItem ? 'Remove from wish list' : 'Add to wish list'}
                             </button>
                         </div>
-                        <div className="w-full md:w-3/4">
-                            {movieInfo.genres &&
-                            movieInfo.genres.map((genre, index) => {
-                                return (
-                                    <p className="inline-block pr-4" key={index}>{genre.name}</p>
-                                )
-                            })
-                            }
-                            <h2 className="text-2xl mt-5">Description</h2>
-                            <p>{movieInfo.overview}</p>
+                        <div className="w-full sm:w-3/4 sm:pl-7">
+                            <div className="hidden sm:inline-block">
+                                {movieInfo.genres &&
+                                movieInfo.genres.map((genre, index) => {
+                                    return (
+                                        <p className="inline-block pr-4" key={index}>{genre.name}</p>
+                                    )
+                                })
+                                }
+                            </div>
+                            <h2 className="text-2xl mt-5 hidden sm:block">Description</h2>
+                            <p className="hidden sm:block">{movieInfo.overview}</p>
                             <h2 className="text-2xl mt-5">Cast</h2>
-                            <div className="grid grid-cols-8">
+                            <div
+                                className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8">
                                 {movieInfo.credits &&
                                 movieInfo.credits.cast.slice(0, 8).map((actor, index) => {
                                     return (

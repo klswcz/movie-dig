dce4a3a726b8a7e19d20211f9b5a8bcd4ef2d0c1
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-03-06 19:56:20 +0000

    update landing page to display properly on mobile devices

--- client/src/components/Home/HeroSection.jsx
+++ client/src/components/Home/HeroSection.jsx
@@ -3,18 +3,19 @@ import {Link} from "react-router-dom";
 function HeroSection() {
     return (
         <div
-            className="h-half-screen bg-gradient-to-br from-green-400 to-blue-500 flex flex-row flex-wrap pt-6 px-32 pb-8">
-            <div className="w-1/2 flex flex-col self-center">
-                <div className="w-full mb-2">
-                    <h1 className="text-white text-6xl font-light">Welcome to MovieDig</h1>
+            className="md:min-h-1/2-screen bg-gradient-to-br from-green-400 to-blue-500 flex flex-row flex-wrap pt-6 px-16 md:px-32 pb-8">
+            <div className="w-full grid xl:grid-cols-3 md:grid-cols-1 my-4 md:my-0">
+            <div className="flex flex-col self-center">
+                    <div className="w-full mb-2">
+                        <h1 className="text-white text-4xl md:text-6xl font-light mb-5 md:mb-0">Welcome to MovieDig</h1>
+                    </div>
+                    <div className="w-full">
+                        <p className="text-white text-lg md:text-2xl font-light">Can't find anything interesting to watch today?
+                            Let us know which films do you like and we'll
+                            hook you up with personalised movie recommendation!</p>
+                    </div>
                 </div>
-                <div className="w-full">
-                    <p className="text-white text-2xl font-light">Can't find anything interesting to watch today?
-                        Let us know which films do you like and we'll
-                        hook you up with personalised movie recommendation!</p>
-                </div>
-            </div>
-            <div className="w-1/2 self-end text-center">
+                <div className="self-end lg:text-left xl:text-right mt-5 md:mt-0">
                     <span className="text-white text-lg">
                     <Link to="/login"
                           className="font-bold">Log in</Link>
@@ -22,6 +23,7 @@ function HeroSection() {
                     <Link to="/register"
                           className="bg-gray-900 px-3 py-2 rounded-md">Register</Link>
                     </span>
+                </div>
             </div>
         </div>
     )

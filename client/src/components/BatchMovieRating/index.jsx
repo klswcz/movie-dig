7536aedbc1200be95c97d6a059b50079589745fc
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-04-03 20:35:07 +0100

    Add button on batch rating page for redirecting to the Dashboard

--- client/src/components/BatchMovieRating/index.jsx
+++ client/src/components/BatchMovieRating/index.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
 import {get as getRatingPropositionService} from "../../services/RatingPropositionService";
 import MovieCard from "../UI/MovieCard";
+import Button from "../UI/Form/Button";
 
 function BatchMovieRating(props) {
     let history = useHistory();
@@ -21,14 +22,16 @@ function BatchMovieRating(props) {
 
 
     return (
-        <div className="h-screen-minus-navbar pb-14 px-4 mt-16">
+        <div className="h-screen-minus-navbar pb-4 px-4 mt-16">
             {loadingFinished ?
                 (<>
                     <h1 className="text-4xl pt-5 sm:mb-3">Let's rate some movies!</h1>
                     <div className="flex flex-wrap">
                         {propositions.map((proposition, propositionIndex) => {
                             return (
-                                <div className="grid w-full 4xl:grid-cols-12 3xl:grid-cols-10 2xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 grid-cols-2" key={propositionIndex}>
+                                <div
+                                    className="grid w-full 4xl:grid-cols-12 3xl:grid-cols-10 2xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 grid-cols-2"
+                                    key={propositionIndex}>
                                     <p>{proposition.name}</p>
                                     {proposition.movies.map((movie, index) => {
                                         return (
@@ -46,6 +49,9 @@ function BatchMovieRating(props) {
                                 </div>
                             )
                         })}
+                    </div>
+                    <div className="mt-5">
+                        <Button label={'Go to Dashboard'} widthAuto={true} to={'/dashboard'}/>
                     </div>
                 </>) :
                 <p>Loading</p>}
--- client/src/components/UI/Form/Button.jsx
+++ client/src/components/UI/Form/Button.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Button(props) {
 
-    return (
-        <button type="submit" onClick={props.onClick}
-                className={"group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 " + props.customClass }>
+    const button = (
+        <button type={props.to ? 'button' : 'submit'} onClick={props.onClick}
+                className={`${props.customClass} ${props.widthAuto ? 'w-auto' : 'w-full'} group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
           <span className="absolute left-0 inset-y-0 flex items-center pl-3">
           </span>
             {props.label}
         </button>
+    )
+
+    return (
+        props.to ?
+            <Link to={props.to}>
+                {button}
+            </Link>
+            :
+            button
     )
 }
 

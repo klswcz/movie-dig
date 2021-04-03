46d7f5c0e717206ef59d98b8d30a3e28a0d8b249
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-04-04 00:16:29 +0100

    Update MovieCard component styling

--- client/src/components/UI/MovieCard.jsx
+++ client/src/components/UI/MovieCard.jsx
@@ -31,60 +31,62 @@ function MovieCard(props) {
         })
     }
 
-    const movieLabel = (
-        <div className={"px-2 bg-white inset-x-0 bottom-0 absolute " + (props.disableLink ? '' : 'hover:underline')}>
-            <p className="text-center text-md font-medium">{props.title}</p>
-            {!props.hideRatings &&
-            <div className="grid grid-cols-2 text-center mb-2">
-                <div>
-                    <p className="text-xs font-medium hover:no-underline">
-                        <FontAwesomeIcon icon={faStar}/>
-                        <span> {Math.round(props.voteAverage / 2 * 10) / 10}</span>
-                    </p>
+    const movieCard = (
+        <div
+            className={`relative bg-white border border-black shadow overflow-hidden sm:rounded-md h-72  ${(props.disableLink ? 'mx-2 my-3' : '')}`}
+            style={{
+                backgroundImage: props.posterPath ? 'url(https://image.tmdb.org/t/p/w300' + props.posterPath + ')' : 'url(https://dummyimage.com/300x450/cccccc/ffffff&text=No+poster)',
+                backgroundPosition: "center",
+                backgroundSize: "cover"
+            }}>
+            <div
+                className={"px-2 bg-white inset-x-0 bottom-0 absolute "}>
+                <p className="text-center text-md font-medium">{props.title}</p>
+                {!props.hideRatings &&
+                <div className="grid grid-cols-2 text-center mb-2">
+                    <div>
+                        <p className="text-xs font-medium">
+                            <FontAwesomeIcon icon={faStar}/>
+                            <span> {Math.round(props.voteAverage / 2 * 10) / 10}</span>
+                        </p>
+                    </div>
+                    <div>
+                        <p className="text-xs font-medium">
+                            <FontAwesomeIcon icon={faUser}/>
+                            <span> {props.userRating || '-'}</span>
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p className="text-xs font-medium hover:no-underline">
-                        <FontAwesomeIcon icon={faUser}/>
-                        <span> {props.userRating || '-'}</span>
-                    </p>
-                </div>
-            </div>
-            }
+                }
 
-            {props.showRatingComponent &&
-            <div className="text-center">
-                <ReactStars
-                    className={'inline-block align-bottom'}
-                    count={5}
-                    value={parseFloat(userRating)}
-                    size={20}
-                    onChange={setRating}
-                    color2={'#ffd700'}/>
-                {userRating != null &&
-                <button onClick={deleteRating} className='inline-block ml-4 focus:outline-none align-text-bottom'>
-                    <FontAwesomeIcon icon={faTrash}/>
-                </button>
+                {props.showRatingComponent &&
+                <div className="text-center">
+                    <ReactStars
+                        className={'inline-block align-bottom'}
+                        count={5}
+                        value={parseFloat(userRating)}
+                        size={20}
+                        onChange={setRating}
+                        color2={'#ffd700'}/>
+                    {userRating != null &&
+                    <button onClick={deleteRating} className='inline-block ml-4 focus:outline-none align-text-bottom'>
+                        <FontAwesomeIcon icon={faTrash}/>
+                    </button>
+                    }
+                </div>
                 }
             </div>
-            }
         </div>
+
     )
 
     return (
-        <div className="relative bg-white border border-black shadow overflow-hidden sm:rounded-md mx-2 my-3 h-72"
-             style={{
-                 backgroundImage: props.posterPath ? 'url(https://image.tmdb.org/t/p/w300' + props.posterPath + ')' : 'url(https://dummyimage.com/300x450/cccccc/ffffff&text=No+poster)',
-                 backgroundPosition: "center",
-                 backgroundSize: "cover"
-             }}>
-            {props.disableLink ?
-                movieLabel
-                :
-                <Link to={{pathname: '/movie', search: `?id=${props.movieId}`}} className="mx-2 my-3">
-                    {movieLabel}
-                </Link>
-            }
-        </div>
+        props.disableLink ?
+            movieCard
+            :
+            <Link to={{pathname: '/movie', search: `?id=${props.movieId}`}} className="mx-2 my-3">
+                {movieCard}
+            </Link>
     )
 }
 

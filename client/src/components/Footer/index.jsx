4a8c77eceb35738c96b7bf2c307546f1faa0b3ab
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-04-25 14:31:34 +0100

    Add links to GitHub and LinkedIn to the footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,7 +1,16 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelopeOpenText} from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
     return (
         <div id="footer" className="w-full h-14 bg-blue-500 text-center text-white pt-3 absolute bottom-0">
-            <p>Damian Klisiewicz / 2020</p>
+            <p>Damian Klisiewicz / 2021 /
+                <a  className="ml-2" href="https://github.com/klswcz" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
+                <a  className="ml-2" href="https://www.linkedin.com/in/damian-klisiewicz/" target="_blank"><FontAwesomeIcon
+                    icon={faLinkedin}/></a>
+                <a  className="ml-2" href="mailto:damianklisiewicz@gmail.com"><FontAwesomeIcon icon={faEnvelopeOpenText}/></a>
+            </p>
         </div>
     )
 }

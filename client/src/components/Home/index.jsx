80012ee6e3f3fa5281845686364e1baa19ec0ee5
Damian Klisiewicz <damianklisiewicz@gmail.com>
2021-05-01 13:18:48 +0100

    Update favicon and homepage wording

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -20,6 +20,27 @@ function Home() {
         <div className=" pb-14">
             <HeroSection/>
             <div className="flex flex-row flex-wrap pt-12 px-10 xl:px-20">
+                <div className="w-full grid grid-cols-1 xl:grid-cols-2 mb-12">
+                    <div className="sm:mx-16">
+                        <h2 className="text-2xl mt-5 font-extrabold text-gray-900 mb-1">How are recommendations
+                            generated?</h2>
+                        <p className="text-justify">I have built MovieDig with the implementation of the Collaborative
+                            Filtering recommendation system. What it means is that the algorithm will use your movie
+                            ratings to match your profile with users who have a taste in movies similar to yours. As a
+                            result, you will be presented with productions that others have enjoyed, and I believe you
+                            will like them too.</p>
+                    </div>
+                    <div className="sm:mx-16">
+                        <h2 className="text-2xl mt-5 font-extrabold text-gray-900 mb-1">Privacy</h2>
+                        <p className="text-justify">The only data that MovieDig collects about the user are ratings and
+                            movies added to wishlists. There are no ads or tracking tools. You have complete control
+                            over your information and can completely delete your profile with all the stored activity
+                            immediately at any time.</p>
+                    </div>
+                </div>
+                <div className="w-full grid-cols-1 sm:mx-16">
+                    <h2 className="text-2xl mt-5 font-extrabold text-gray-900 mb-1">Features showcase</h2>
+                </div>
                 <div className="w-full grid xl:grid-cols-4 md:grid-cols-2">
                     <FeatureShowcase image={recommendations}
                                      description={"Never trouble with picking the movie again. Let us help you with that and get accurate recommendations, based on your account's history, delivered to you by our custom-built recommender script."}/>
@@ -28,7 +49,7 @@ function Home() {
                     <FeatureShowcase image={rating}
                                      description={"Rate movies you've seen and add those you are interested in to a wish list for better organisation."}/>
                     <FeatureShowcase image={dashboard}
-                                     description={"Enjoy using our modern and responsive website. Access it on a go with a mobile version."}/>
+                                     description={"Enjoy using our modern and responsive website. Access it on a go with the mobile version of the application."}/>
                 </div>
             </div>
         </div>

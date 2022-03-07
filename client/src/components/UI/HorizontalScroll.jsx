4c138a9d5cbba22d4a7271d8c4cae2a1c7e304d0
Damian Klisiewicz <damianklisiewicz@gmail.com>
2022-03-07 15:33:05 +0100

    Update styling of horizontal scroll arrows

--- client/src/components/UI/HorizontalScroll.jsx
+++ client/src/components/UI/HorizontalScroll.jsx
@@ -5,9 +5,9 @@ import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 function HorizontalScroll(props) {
     return (
         <ScrollMenu
-            arrowLeft={<FontAwesomeIcon icon={faChevronLeft} className="h-full"/>}
-            arrowRight={<FontAwesomeIcon icon={faChevronRight} className="h-full"/>}
-            arrowClass={'px-5 h-72 flex bg-indigo-100 hover:bg-indigo-600 text-indigo-600 hover:text-indigo-50 duration-500 cursor-pointer'}
+            arrowLeft={<FontAwesomeIcon icon={faChevronLeft}/>}
+            arrowRight={<FontAwesomeIcon icon={faChevronRight}/>}
+            arrowClass={'px-5 h-72 flex bg-indigo-100 hover:bg-indigo-600 text-indigo-600 hover:text-indigo-50 duration-500 cursor-pointer flex items-center'}
             wheel={false}
             dragging={false}
             clickWhenDrag={false}
